fdd9f91ecd387b5b95902be665be2aa637a67238
yashwanth008 <76914360+yashwanth008@users.noreply.github.com>
2025-09-15 15:15:00 -0700

    Update rondo.js

--- rondo.js
+++ rondo.js
@@ -1,6 +1,6 @@
 
 //spreadsheet variables - edit these to point to your spreadsheet
-var spreadsheetID = "1iwIx8sqiBTqbtAI7TX4vDGDVee3dPz0wa3MazBv2fzQ";
+var spreadsheetID = "2PACX-1vQUxn1pCicjYVylfqN7eMZTbpVLPI4Q0YfhnRTRJtkliZtnjWkXxF4U4OLgsDnuQozWiDgFUXbsDwBe";
 var siteSheet ='1569296108';
 var pagesSheet = '28080804';
 var itemsSheet = '0';
